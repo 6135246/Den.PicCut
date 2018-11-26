77c7f9a09f1ff2ff7a92645b69353e4da0abf2cd
Herian <6135246@qq.com>
2018-11-26 16:34:15 +0800

    Initial commit

---
+++ Den.PicCut/frmMain.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Den.PicCut
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCompress = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtBaseHeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtBaseWidth = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSelectDir = new System.Windows.Forms.Button();
+            this.btnSelectImg = new System.Windows.Forms.Button();
+            this.txtSaveDir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtImgSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCompress);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.txtBaseHeight);
+            this.panel1.Controls.Add(this.txtHeight);
+            this.panel1.Controls.Add(this.txtBaseWidth);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnSelectDir);
+            this.panel1.Controls.Add(this.btnSelectImg);
+            this.panel1.Controls.Add(this.txtSaveDir);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtImgSource);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(632, 135);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Location = new System.Drawing.Point(509, 101);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(75, 23);
+            this.btnCompress.TabIndex = 7;
+            this.btnCompress.Text = "等比压缩";
+            this.btnCompress.UseVisualStyleBackColor = true;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(509, 68);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "开始裁剪";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtBaseHeight
+            // 
+            this.txtBaseHeight.Enabled = false;
+            this.txtBaseHeight.Location = new System.Drawing.Point(309, 105);
+            this.txtBaseHeight.Name = "txtBaseHeight";
+            this.txtBaseHeight.Size = new System.Drawing.Size(100, 21);
+            this.txtBaseHeight.TabIndex = 4;
+            this.txtBaseHeight.Text = "800";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(309, 76);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 21);
+            this.txtHeight.TabIndex = 4;
+            this.txtHeight.Text = "800";
+            // 
+            // txtBaseWidth
+            // 
+            this.txtBaseWidth.Enabled = false;
+            this.txtBaseWidth.Location = new System.Drawing.Point(110, 107);
+            this.txtBaseWidth.Name = "txtBaseWidth";
+            this.txtBaseWidth.Size = new System.Drawing.Size(100, 21);
+            this.txtBaseWidth.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(110, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(241, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "原始高度：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "原始宽度：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "最大高度：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "最大宽度：";
+            // 
+            // btnSelectDir
+            // 
+            this.btnSelectDir.Location = new System.Drawing.Point(508, 40);
+            this.btnSelectDir.Name = "btnSelectDir";
+            this.btnSelectDir.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectDir.TabIndex = 2;
+            this.btnSelectDir.Text = "选择";
+            this.btnSelectDir.UseVisualStyleBackColor = true;
+            this.btnSelectDir.Click += new System.EventHandler(this.btnSelectDir_Click);
+            // 
+            // btnSelectImg
+            // 
+            this.btnSelectImg.Location = new System.Drawing.Point(509, 9);
+            this.btnSelectImg.Name = "btnSelectImg";
+            this.btnSelectImg.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectImg.TabIndex = 2;
+            this.btnSelectImg.Text = "选择";
+            this.btnSelectImg.UseVisualStyleBackColor = true;
+            this.btnSelectImg.Click += new System.EventHandler(this.btnSelectImg_Click);
+            // 
+            // txtSaveDir
+            // 
+            this.txtSaveDir.Location = new System.Drawing.Point(109, 44);
+            this.txtSaveDir.Name = "txtSaveDir";
+            this.txtSaveDir.Size = new System.Drawing.Size(392, 21);
+            this.txtSaveDir.TabIndex = 1;
+            this.txtSaveDir.Text = "F:\\微信商城\\cut";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "文件保存位置：";
+            // 
+            // txtImgSource
+            // 
+            this.txtImgSource.Location = new System.Drawing.Point(110, 13);
+            this.txtImgSource.Name = "txtImgSource";
+            this.txtImgSource.Size = new System.Drawing.Size(392, 21);
+            this.txtImgSource.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "需要裁剪的文件：";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 135);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(632, 449);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 584);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.Text = "图片切割工具(6135246@qq.com)";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtImgSource;
+        private System.Windows.Forms.Button btnSelectImg;
+        private System.Windows.Forms.Button btnSelectDir;
+        private System.Windows.Forms.TextBox txtSaveDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtBaseHeight;
+        private System.Windows.Forms.TextBox txtBaseWidth;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCompress;
+    }
+}
+
---
+++ Den.PicCut/frmMain.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Den.PicCut
+{
+    public partial class frmMain : Form
+    {
+        const int SRCCOPY = 0x00CC0020;
+        int mMaxSizeX, mMaxSizeY;
+        string mFileName, mFileExt;
+        bool mIsCompress = false;
+        bool mIsStarted = false;
+        string mStrDirect = "";
+        delegate void UpdateReportCallBack(string pReport);
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectImg_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtImgSource.Text = openFileDialog1.FileName;
+                FileInfo oFil = new FileInfo(txtImgSource.Text);
+                mFileName = oFil.Name.ToLower().Replace(oFil.Extension.ToLower(),"");
+                mFileExt = oFil.Extension;
+                Image oImage = Bitmap.FromFile(txtImgSource.Text);
+                txtBaseWidth.Text = oImage.Width.ToString();
+                txtBaseHeight.Text= oImage.Height.ToString();
+                oImage.Dispose();
+            }
+        }
+
+        private void btnSelectDir_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSaveDir.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+      
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtImgSource.Text.Trim()) )
+            {
+                MessageBox.Show("需要裁剪的文件不能为空");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtSaveDir.Text.Trim()))
+            {
+
+                MessageBox.Show("裁剪后的文件路径不能为空！");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtHeight.Text.Trim()))
+            {
+
+                MessageBox.Show("文件高度不能为空！");
+                return;
+            }
+            if (!File.Exists(txtImgSource.Text))
+            {
+                MessageBox.Show("文件不存在！");
+
+                return;
+            }
+            richTextBox1.Text = "";
+
+            if (mIsStarted)
+            {
+                mIsStarted = false;
+                btnSelectDir.Enabled = true;
+                btnSelectImg.Enabled = true;
+                btnStart.Text = "开始裁剪";
+            }
+            else
+            {
+               
+                mIsStarted = true;                
+                btnStart.Text = "停止裁剪";
+                btnSelectDir.Enabled = false;
+                btnSelectImg.Enabled = false;
+                Thread oThread = new Thread(new ThreadStart(StartCut));
+                oThread.Start();
+            }
+        }
+       
+        private void StartCut()
+        {
+            mStrDirect = txtSaveDir.Text.Trim() + "\\" + mFileName + "\\";
+            if (!Directory.Exists(mStrDirect))
+            {
+                Directory.CreateDirectory(mStrDirect);
+            }
+            int oHeight = Convert.ToInt32(txtHeight.Text);
+            int oIndex = 0;
+            int oCutHeight = 0;
+            Image oImage = Bitmap.FromFile(txtImgSource.Text);
+            mMaxSizeX = oImage.Width;
+            mMaxSizeY = oImage.Height;
+            UpdateReport("开始裁剪！");
+            while (mIsStarted)
+            {
+                if (mMaxSizeY <= oHeight)
+                {
+                    oHeight = mMaxSizeY;
+                    mIsStarted = false;
+                    UpdateReport("全部生成完成！");
+                }
+                
+                Cut(oIndex, 0, oCutHeight, oImage.Width, oHeight, oImage.Width, oImage.Height, oImage);
+                mMaxSizeY = mMaxSizeY - oHeight;
+                oCutHeight += oHeight;
+                oIndex += 1;
+            }
+            oImage.Dispose();
+        }
+       
+        public void Cut(int pIndex, int pStartX, int pStartY, int pWidth, int pHeight, int pDefaultWidth, int pDefaultHeight, Image pBaseImage)
+        {
+            int i_x = pStartX,               // 裁切开始横坐标
+                        i_y = pStartY,               // 裁切开始纵坐标
+                        i_w = pWidth,               // 裁切宽度
+                        i_h = pHeight,               // 裁切高度
+                        i_boundx = pDefaultWidth,     // 页面图片初始宽度
+                        i_boundy = pDefaultHeight,     // 页面图片初始高度
+                        s_w = pBaseImage.Width,                                           // 图片原始宽度
+                        s_h = pBaseImage.Height;                                          // 图片原始高度
+            double scale = 1,                                                   // 实际比例
+                s_scale = 1;                                                    // 伸缩比例
+
+            // 页面显示图片时，最大宽度和高度为400，所以用400进行比较
+            if (s_w > 400 || s_h > 400)
+            {
+                // 需要计算伸缩比例
+                if (s_w >= s_h)
+                {
+                    // 图片宽度大于高度，按宽度伸缩比例计算
+                    s_scale = double.Parse(i_boundx.ToString()) / 400.00;
+                    s_scale = double.Parse(s_scale.ToString("#0.00"));
+                    // 计算实际比例
+                    scale = double.Parse(s_w.ToString()) / 400.00;
+                    scale = double.Parse(scale.ToString("#0.00"));
+                }
+                else
+                {
+                    // 图片高度大于宽度，按高度伸缩比例计算
+                    s_scale = double.Parse(i_boundy.ToString()) / 400.00;
+                    s_scale = double.Parse(s_scale.ToString("#0.00"));
+                    // 计算实际比例
+                    scale = double.Parse(s_h.ToString()) / 400.00;
+                    scale = double.Parse(scale.ToString("#0.00"));
+                }
+            }
+
+            // 计算实际的裁切开始位置和宽高
+            int t_x = Convert.ToInt32(i_x / s_scale * scale),
+                t_y = Convert.ToInt32(i_y / s_scale * scale),
+                t_w = Convert.ToInt32(i_w / s_scale * scale),
+                t_h = Convert.ToInt32(i_h / s_scale * scale);
+            Bitmap destBitmap = null;
+            // 目标图容器
+            Rectangle destRect = new Rectangle();
+            // 原图截取区域
+            Rectangle srcRect = new Rectangle();
+            System.Drawing.Imaging.ImageFormat iformat;
+            // 目标图
+            destBitmap = new Bitmap(t_w, t_h);
+            // 目标图容器
+            destRect = new Rectangle(0, 0, t_w, t_h);
+            // 原图截取区域
+            srcRect = new Rectangle(t_x, t_y, t_w, t_h);
+            //从指定的Image对象创建新Graphics对象
+            Graphics graphics = Graphics.FromImage(destBitmap);
+            switch (mFileExt.ToUpper())
+            {
+                case ".GIF":
+                    iformat = System.Drawing.Imaging.ImageFormat.Gif;
+                    graphics.Clear(Color.Transparent);
+                    break;
+                case ".JPG":
+                case ".JPEG":
+                    iformat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                    graphics.Clear(Color.White);
+                    break;
+                case ".PNG":
+                    iformat = System.Drawing.Imaging.ImageFormat.Png;
+                    graphics.Clear(Color.Transparent);
+                    break;
+                case ".BMP":
+                    iformat = System.Drawing.Imaging.ImageFormat.Bmp;
+                    graphics.Clear(Color.White);
+                    break;
+                default:
+                    iformat = System.Drawing.Imaging.ImageFormat.Png;
+                    graphics.Clear(Color.Transparent);
+                    break;
+            }
+            //设置质量
+            graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            string imageSavePath = mStrDirect + pIndex.ToString() + mFileExt;
+            if (mIsCompress)
+            {
+                imageSavePath = txtSaveDir.Text+"\\"+mFileName+mFileExt;
+            }
+            //在指定位置并且按指定大小绘制原图片对象
+            graphics.DrawImage(pBaseImage, destRect, srcRect, GraphicsUnit.Pixel);
+            EncoderParameters ep = new EncoderParameters(2);
+            ep.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 70L);
+            ep.Param[1] = new EncoderParameter(System.Drawing.Imaging.Encoder.ColorDepth, 48 + 24L);
+            ImageCodecInfo ici = this.getImageCoderInfo(mFileExt);
+            destBitmap.Save(imageSavePath, ici, ep);
+            UpdateReport(imageSavePath);
+        }
+        /// <summary>
+        /// 获取图片编码类型信息
+        /// </summary>
+        /// <param name="coderType">编码类型</param>
+        /// <returns>ImageCodecInfo</returns>
+        private ImageCodecInfo getImageCoderInfo(string pExt)
+        {
+            ImageCodecInfo[] iciS = ImageCodecInfo.GetImageEncoders();
+            ImageCodecInfo retIci = null;
+            foreach (ImageCodecInfo ici in iciS)
+            {
+                if (ici.FilenameExtension.Contains(pExt.ToUpper()))
+                {
+                    retIci = ici;
+
+                }
+            }
+            return retIci;
+        }
+        /// <summary>
+        /// 状态报告
+        /// </summary>
+        /// <param name="report"></param>
+        private void UpdateReport(string pReport)
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateReportCallBack cb = new UpdateReportCallBack(UpdateReport);
+                try
+                {
+                    this.Invoke(cb, pReport);
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                if (pReport.Equals("全部生成完成！"))
+                {
+                    btnSelectDir.Enabled = true;
+                    btnSelectImg.Enabled = true;
+                    btnStart.Text = "开始裁剪";
+                }
+                if (string.IsNullOrWhiteSpace(richTextBox1.Text.Trim()))
+                {
+                    richTextBox1.Text = pReport;
+                }
+                else
+                {
+                    richTextBox1.Text = richTextBox1.Text + "\r\n" + pReport;
+                }
+            }
+        }
+
+        private void StartCompress()
+        {
+            mStrDirect = txtSaveDir.Text.Trim() + "\\" + mFileName + "\\";
+            if (!Directory.Exists(mStrDirect))
+            {
+                Directory.CreateDirectory(mStrDirect);
+            }
+           
+            int oIndex = 0;
+            int oCutHeight = 0;
+            Image oImage = Bitmap.FromFile(txtImgSource.Text);
+            mMaxSizeX = oImage.Width;
+            mMaxSizeY = oImage.Height;
+            int oHeight = oImage.Height;
+            UpdateReport("开始压缩！");
+            while (mIsStarted)
+            {
+                if (mMaxSizeY <= oHeight)
+                {
+                    oHeight = mMaxSizeY;
+                    mIsStarted = false;
+                    UpdateReport("全部生成完成！");
+                }
+
+                Cut(oIndex, 0, oCutHeight, oImage.Width, oHeight, oImage.Width, oImage.Height, oImage);
+                mMaxSizeY = mMaxSizeY - oHeight;
+                oCutHeight += oHeight;
+                oIndex += 1;
+            }
+            oImage.Dispose();
+        }
+       
+        private void btnCompress_Click(object sender, EventArgs e)
+        {
+            mIsCompress = true;
+            mIsStarted = true;
+            StartCompress();
+            mIsCompress = false;
+        }
+    }
+}
